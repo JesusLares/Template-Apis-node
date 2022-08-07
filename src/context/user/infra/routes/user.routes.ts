d1ac296bf77c02ad00a1e758e1525cc3493a1297
Jesus Lares <jesus.lares2000@gmail.com>
2022-08-07 07:44:15 -0500

    refactor: remove comments

--- src/context/user/infra/routes/user.routes.ts
+++ src/context/user/infra/routes/user.routes.ts
@@ -6,29 +6,6 @@ import UserController from "../controller/user.controller";
 const URL_BASE = `${env.initialRoute}/client`;
 const clientController = new UserController();
 
-/**
- * Post track
- * @openapi
- * /api/v1/users:
- *  post:
- *    tags:
- *      - users
- *    sumary: "Get user"
- *    description: Get track detail of user
- *    requestBody:
- *      content:
- *        aplication/json:
- *          schema:
- *            $ref: "#/components/schemas/user"
- *    responses:
- *      '200':
- *        description: Retorna el objeto con el campo de user.
- *      '404':
- *        description: Error de validacion
- *    segurity:
- *      - bearerAuth: []
- */
-
 const routes: IRoute[] = [
   {
     path: URL_BASE,
