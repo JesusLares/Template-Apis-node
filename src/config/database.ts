818023d1c12a6d75562f268bea04d31513a09333
Jesus Lares <jesus.lares2000@gmail.com>
2022-08-17 05:41:24 -0500

    refactor: add disable line for eslint

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
 import env from "./env";
 
+// eslint-disable-next-line object-curly-newline
 const { database, username, password, storage } = env;
 
 const sequelize = new Sequelize(database, username, password, {
