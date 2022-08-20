350567ffde6f8844c23d3633edb7370bccebee80
Jesus Lares <jesus.lares2000@gmail.com>
2022-08-20 09:10:42 -0500

    test: add testing for use cases

---
+++ __tests__/user/findAllUser.spec.ts
@@ -0,0 +1,28 @@
+import sequelize from "../../src/config/database";
+import UserSchema from "../../src/context/user/domain/UserSchema";
+
+import CreateUser from "../../src/context/user/infra/useCases/createUser";
+import FindUser from "../../src/context/user/infra/useCases/findAllUser";
+import UserFactory from "../mocks/factories/userFactory";
+
+beforeAll(() => sequelize.sync());
+beforeEach(() => UserSchema.destroy({ truncate: true }));
+afterAll(() => sequelize.close());
+
+describe("Find all user useCase", () => {
+  const userMock = UserFactory.createDefault();
+  const findUser = new FindUser();
+  const createUser = new CreateUser();
+
+  test("should return all user type in the db", async () => {
+    await createUser.exec(userMock);
+    const userMocks = [userMock];
+    const user = await findUser.exec({});
+    expect(user.length).toBe(userMocks.length);
+  });
+
+  test("should return an empty array if there arent user", async () => {
+    const user = await findUser.exec({});
+    expect(user.length).toBe(0);
+  });
+});
