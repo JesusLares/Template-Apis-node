7854a4bc9334ad5fc71cf064ae2f6717df77cfaa
Jesus-Lares <jesus.lares2000@gmail.com>
2023-09-25 09:06:23 -0600

    chore: config paths to jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig.paths.json");
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   cacheDirectory: ".tmp/jestCache",
-  moduleNameMapper: {
-    "@context/(.*)": "<rootDir>/src/context/$1",
-    "@router/(.*)": "<rootDir>/src/router/$1",
-    "@config/(.*)": "<rootDir>/src/config/$1",
-    "@interface/(.*)": "<rootDir>/src/interface/$1",
-    "@middlewares/(.*)": "<rootDir>/src/middlewares/$1",
-    "@utils/(.*)": "<rootDir>/src/utils/$1",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
   },
+  testMatch: ["**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" }),
+  transform: {
+    ".spec.ts$": "ts-jest",
+  },
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
+    },
+  },
+  coverageReporters: ["json", "lcov", "text", "clover"],
+  roots: ["<rootDir>/src", "<rootDir>/__tests__"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
